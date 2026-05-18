41c522a70772391fd170950ee02012008a06f904
Ivan Vecera <ivecera@redhat.com>
2026-05-18 14:50:23 +0200

    dpll: zl3073x: add channel TIE write operation
    Add TIE phase adjustment operation zl3073x_chan_tie_write() that
    converts nanoseconds to TIE units (0.01 ps) and writes TIE data
    to the specified DPLL channel. A tie_lock mutex serializes TIE
    operations.
    
    Add convenience helper zl3073x_chan_mode_supports_tie() to chan.h.
    
    Tested-by: Chris du Quesnay <Chris.duQuesnay@microchip.com>
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>

--- drivers/dpll/zl3073x/core.h
+++ drivers/dpll/zl3073x/core.h
@@ -27,6 +27,7 @@ struct zl3073x_dpll;
 #define ZL_POLL_MB_TIMEOUT_US		(30 * USEC_PER_MSEC)
 #define ZL_POLL_PHASE_ERR_TIMEOUT_US	(50 * USEC_PER_MSEC)
 #define ZL_POLL_PHASE_STEP_TIMEOUT_US	(3000 * USEC_PER_MSEC)
+#define ZL_POLL_TIE_WR_TIMEOUT_US	(1000 * USEC_PER_MSEC)
 #define ZL_POLL_TOD_RD_TIMEOUT_US	(30 * USEC_PER_MSEC)
 #define ZL_POLL_TOD_WR_TIMEOUT_US	(1000 * USEC_PER_MSEC)
 
@@ -58,6 +59,7 @@ struct zl3073x_chip_info {
  * @regmap: regmap to access device registers
  * @info: detected chip info
  * @multiop_lock: to serialize multiple register operations
+ * @tie_lock: to serialize TIE write operations
  * @phase_step_lock: to serialize output phase step operations
  * @ref: array of input references' invariants
  * @out: array of outs' invariants
@@ -75,6 +77,7 @@ struct zl3073x_dev {
 	struct regmap			*regmap;
 	const struct zl3073x_chip_info	*info;
 	struct mutex			multiop_lock;
+	struct mutex			tie_lock;
 	struct mutex			phase_step_lock;
 
 	/* Invariants */
